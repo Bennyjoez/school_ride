a679f94979f8999cf8c6a5c87910693ad2d221a6
Benson Njuguna <njugunab655@gmail.com>
2026-06-26 18:16:03 +0300

    commit
    chore: prepare frontend for GitHub pages deployment

--- school_ride_ui/vite.config.js
+++ school_ride_ui/vite.config.js
@@ -4,14 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': 'http://127.0.0.1:8000',
-      '/ws': {
-        target: 'ws://127.0.0.1:8000',
-        ws: true,
-      },
-    },
+    base: 'school_ride', // Set the base URL for the stage and production server
   },
   resolve: {
     alias: {
