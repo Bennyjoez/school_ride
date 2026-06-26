86695ec934ea6788560b8a783ac890fc72d7cacd
Benson Njuguna <njugunab655@gmail.com>
2026-06-26 15:58:58 +0300

    chore: prepare backend for Render deployment

--- school_ride_api/api/settings.py
+++ school_ride_api/api/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "apps.trips",
     "apps.notifications",
     "apps.tracking",
+    "corsheaders",
 ]
 
 
@@ -65,6 +66,9 @@ INSTALLED_APPS = [
 # =============================================================================
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # must be before CommonMiddleware
+    'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,6 +77,14 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'https://Bennyjoez.github.io',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # =============================================================================
